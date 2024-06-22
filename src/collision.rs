66293986747b372025773630b184884eaecd0551
tbergqvist <tim@piktiv.se>
2024-06-22 17:33:35 +0200

    refactored collision check

--- src/collision.rs
+++ src/collision.rs
@@ -1,28 +1,31 @@
-use bevy::{math::bounding::{Aabb2d, IntersectsVolume}, prelude::{Commands, Entity, Query, With, Without}, transform::components::Transform};
+use bevy::{math::bounding::{Aabb2d, IntersectsVolume}, prelude::{Commands, Entity, Query}, transform::components::Transform};
 
 use crate::components::{Hitable, Projectile};
 
 pub fn check_collisions(
 		mut commands: Commands,
-		hitables: Query<(Entity, &Transform, &Hitable), Without<Projectile>>,
-		projectile: Query<(Entity, &Transform, &Hitable), With<Projectile>>
+		hitables: Query<(Entity, &Transform, &Hitable, Option<&Projectile>)>
 ) {
-	for (projectile_entity, projectile_transform, projectile_hitable) in &mut projectile.iter() {
-		let projectile_aabb = Aabb2d::new(
-			projectile_transform.translation.truncate(), 
-			projectile_hitable.size / 2.
-		);
-		
-		for (hitable_entity, hitable_transform, hitable) in &mut hitables.iter() {
-			let hitable_aabb = Aabb2d::new(
-				hitable_transform.translation.truncate(), 
-				hitable.size / 2.
-			);
+	let mut iter = hitables.iter_combinations();
 
-			if projectile_aabb.intersects(&hitable_aabb) {
-				commands.entity(projectile_entity).despawn();
-				commands.entity(hitable_entity).despawn();
-			}
+	while let Some([(entity, transform, hitable, projectile), (entity2, transform2, hitable2, projectile2)]) = iter.fetch_next()  {
+		if projectile.is_none() && projectile2.is_none() {
+			continue;
+		}
+
+		let hitable_aabb = Aabb2d::new(
+			transform.translation.truncate(), 
+			hitable.size / 2.
+		);
+
+		let projectile_aabb = Aabb2d::new(
+			transform2.translation.truncate(), 
+			hitable2.size / 2.
+		);
+
+		if projectile_aabb.intersects(&hitable_aabb) {
+			commands.entity(entity2).despawn();
+			commands.entity(entity).despawn();
 		}
 	}
 }
